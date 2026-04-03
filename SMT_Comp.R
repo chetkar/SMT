d326957fae79c169189045b6d0098dc32e35ec08
Chetkar Jha <chetkar@gmail.com>
2026-04-03 19:14:27 +0530

    Update SMT_Comp.R

--- SMT_Comp.R
+++ SMT_Comp.R
@@ -291,8 +291,8 @@ smt_A <- function(A, K_m, method="1",alpha=0.05){
       B.est <- matrix(0, k, k)
       B.est  <- get.Best(A,g)
 
-      B.est[which(B.est<0.05)]=0.05
-      B.est[which(B.est > 0.95)]=0.95
+      B.est[which(B.est<0.1)]=0.1
+      B.est[which(B.est > 0.9)]=0.9
       tvec      <- rep(0, k)
 
   		for(l in 1:k){
